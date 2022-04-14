7dc79a3a19a1cdeaa4440150dc92335509dd81d5
gamergunk <97487382+gamergunk@users.noreply.github.com>
2022-04-13 19:38:08 -0500

    moved animation

--- sketch.js
+++ sketch.js
@@ -1,104 +1,17 @@
-let img1;
-let img4;
-let img5;
-let img6;
-let img7;
-let img8;
-let img9;
-let img10;
-let img11;
-let img12;
-let img13;
-let img14;
-let img15;
-let header;
-let eugenia;
-let drugs;
-let euphoria;
-let gecs;
-let christianity;
-let cryptocussy;
-let missouri;
-let government;
-let stinky;
-let dragons;
-let brady;
-let gay;
-let kanye;
-
-function preload(){
-  img1 = createImg('https://i.imgur.com/kHagEFR.jpg', 'eugenia cooney');
-  img4 = createImg('https://klyker.com/wp-content/uploads/2015/07/woman-with-insane-body-modifications-6.jpg', 'dylan brady');
-  img5 = createImg('http://i.huffpost.com/gen/498220/thumbs/r-KETAMINE-DRUG-METHOXETAMINE-large570.jpg', 'ketamine');
-  img6 = createImg('https://tse4.mm.bing.net/th?id=OIP.k2h1nuwwus-yIgySuVt2kwHaEK&pid=Api&P=0&w=302&h=169', 'euphoria');
-  img7 = createImg('https://tse1.mm.bing.net/th?id=OIP.chE_JyrZC8ESGApQ5wisGwAAAA&pid=Api&P=0&w=172&h=172', 'anti christian');
-  img8 = createImg('https://i.ytimg.com/vi/tj4sOjHi7sE/hqdefault.jpg', 'area 51')
-  img9 = createImg('https://youaremom.com/wp-content/uploads/2019/02/siblings-posing-together-smiling.jpg', 'missouri');
-  img10 = createImg('https://www.theladders.com/wp-content/uploads/hug_190429.jpg', 'physical touch');
-  img11 = createImg('http://www.healthcare-online.org/images/10405017/body-odor.jpg', 'stinky');
-  img12 = createImg('https://cdn.vox-cdn.com/thumbor/H2oISYktPQ0lNV0zlWXHQMdQUrE=/0x0:3045x2029/920x613/filters:focal(1293x725:1779x1211):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66025862/1197574316.jpg.0.jpg', 'tom brady');
-  img13 = createImg('https://www.georgianjournal.ge/images/georgianews/2018/March/culture/imagine5.jpg', 'imagine dragons');
-  img14 = createImg('https://tse2.mm.bing.net/th?id=OIP.M9gK090xkEqjzcqm9O19FgHaEK&pid=Api&P=0&w=295&h=166', 'scientology');
-  img15 = createImg('https://images.gmanews.tv/webpics/2020/07/2020-07-08T071142Z_1442775454_RC2VOH95XAFN_RTRMADP_3_USA-ELECTION-KANYE-WEST_2020_07_09_05_53_34.JPG', 'kanye west');
-  header = createImg('assets/myHeader.jpg', 'its a header!');
-  eugenia = createP('Eugenia Cooney dies in armed robbery');
-  drugs = createP("Teenagers are using a new drug that allows them to 'See God'");
-  euphoria = createP('Euphoria creators declare that cellular mitosis is a hoax and will be incorporating that belief into all further episodes');
-  gecs = createP('Dylan Brady of 100 gecs dies at age 31 due to heart failure');
-  christianity = createP('Chance The Rapper denounces Christianity after marrying into the royal family');
-  cryptocussy = createP("New element found near Area 51 scientists are calling 'Cryptocussy'");
-  missouri = createP('Missouri legalizes marraige between immediate family');
-  government = createP('US. Government bans physical contact until further notice');
-  stinky = createP('New studies find that hypnosis can be used to eliminate body odor');
-  dragons = createP('Lead singer of Imagine Dragons arrested for child pornography');
-  brady = createP('Formerly retired NBA quarterback Tom Brady is now working as a middle school art teacher');
-  gay = createP('Historians find evidence to support that founder of Scientology was gay');
-  kanye = createP('President Kanye West declares that America is under Martial Law until his new album is released');
-}
-
 function setup() {
-
-  img1.parent('row1');
-  img4.parent('row3');
-  img5.parent('row1');
-  img6.parent('row1');
-  img7.parent('row3');
-  img8.parent('row3');
-  img9.parent('row5');
-  img10.parent('row5');
-  img11.parent('row7');
-  img12.parent('row9');
-  img13.parent('row7');
-  img14.parent('row9');
-  img15.parent('row9');
-  header.parent('header');
-  img1.size(400, 300);
-  img5.size(400, 300);
-  img6.size(400, 300);
-  eugenia.parent('row2');
-  drugs.parent('row2');
-  euphoria.parent('row2');
-  img4.size(400, 300);
-  img7.size(400, 300);
-  img8.size(400, 300);
-  gecs.parent('row4');
-  christianity.parent('row4');
-  cryptocussy.parent('row4');
-  img9.size(400, 300);
-  img10.size(400, 300);
-  missouri.parent('row6');
-  government.parent('row6');
-  img11.size(400, 300);
-  img13.size(400, 300);
-  stinky.parent('row8');
-  dragons.parent('row8');
-  img12.size(400, 300);
-  img14.size(400, 300);
-  img15.size(400, 300);
-  brady.parent('row10');
-  gay.parent('row10');
-  kanye.parent('row10');
+	createCanvas(400, 400);
+        noLoop();
 }
 
 function draw() {
+	background(255, 127, 50);
+	scale(4);
+
+	if (mouseX < width/2) {
+		fill(255, 0, 0);
+		rect(10, 10, 30, 30);
+	} else {
+		fill(0, 0, 255);
+		rect(10, 10, 30, 30);
+	}
 }
